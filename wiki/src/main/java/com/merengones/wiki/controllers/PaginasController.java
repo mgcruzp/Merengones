d3661e426bdbea7d40a8213fbb95384d77f75ca3
gaby <gcruzpino@gmail.com>
2025-08-02 23:11:52 -0500

    SpringBoot y Thymeleaf

---
+++ wiki/src/main/java/com/merengones/wiki/controllers/PaginasController.java
@@ -0,0 +1,33 @@
+package com.merengones.wiki.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PaginasController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // Busca index.html en /resources/templates/
+    }
+
+    @GetMapping("/equipo")
+    public String equipo() {
+        return "equipo";
+    }
+
+    @GetMapping("/contacto")
+    public String contacto() {
+        return "contacto";
+    }
+
+    @GetMapping("/desarrollo")
+    public String desarrollo() {
+        return "desarrollo";
+    }
+
+    @GetMapping("/pruebas")
+    public String pruebas() {
+        return "pruebas";
+    }
+}
