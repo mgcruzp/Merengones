681bc8e609af58250334e616b6b9117457c3c449
mgcruzp <gcruzpino@gmail.com>
2025-08-25 15:08:42 -0500

    Actualizacion dependencias y creacion entity proceso

--- proyecto/src/main/java/com/web/proyecto/entities/Process.java
+++ proyecto/src/main/java/com/web/proyecto/entities/Process.java
@@ -1,5 +1,36 @@
 package com.web.proyecto.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "process")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Process {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 140)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(length = 50, nullable = false)
+    private String status;
+
+    @OneToMany(mappedBy = "process", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Activity> activities = new ArrayList<>();
+
+    @OneToMany(mappedBy = "process", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Edge> edges = new ArrayList<>();
 }
