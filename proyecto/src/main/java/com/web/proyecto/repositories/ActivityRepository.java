66b6994fb8eb16419e01a5634cee6108e3396c79
Juan Sebastian Vargas <151252035+Jusebbb@users.noreply.github.com>
2025-09-09 12:35:31 -0500

    Update ActivityRepository.java :)

--- proyecto/src/main/java/com/web/proyecto/repositories/ActivityRepository.java
+++ proyecto/src/main/java/com/web/proyecto/repositories/ActivityRepository.java
@@ -2,9 +2,18 @@ package com.web.proyecto.repositories;
 
 import com.web.proyecto.entities.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
+    // existentes
     List<Activity> findByProcess_Id(Long processId);
     boolean existsByNameIgnoreCaseAndProcess_Id(String name, Long processId);
+
+    // NUEVOS filtros “paralelos” a ProcessRepository
+    List<Activity> findByProcess_Empresa_Id(Long empresaId);
+    List<Activity> findByProcess_Status(String status);
+    List<Activity> findByProcess_Category(String category);
+    List<Activity> findByProcess_Empresa_IdAndProcess_Status(Long empresaId, String status);
 }
