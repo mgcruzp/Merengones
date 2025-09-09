e39038ff9f5484e1e4b99303e8ef1a9cbf540151
angepiedrahita87 <angepiedrahita87@gmail.com>
2025-09-08 20:49:58 -0500

    Process 2 juju

--- proyecto/src/main/java/com/web/proyecto/dtos/ProcessDTO.java
+++ proyecto/src/main/java/com/web/proyecto/dtos/ProcessDTO.java
@@ -4,16 +4,22 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProcessDTO {
 
-    @NotNull
     private Long id;
 
     @NotBlank
     private String name;
+
+    private String description;
+
+    private String category;
+
+    private String status;
+
+    private Long empresaId;
 }
--- proyecto/src/main/java/com/web/proyecto/repositories/ProcessRepository.java
+++ proyecto/src/main/java/com/web/proyecto/repositories/ProcessRepository.java
@@ -1,6 +1,17 @@
 package com.web.proyecto.repositories;
 
 import com.web.proyecto.entities.Process;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProcessRepository extends JpaRepository<Process, Long> { }
+public interface ProcessRepository extends JpaRepository<Process, Long> {
+    List<Process> findByCompanyId(Long companyId);
+
+    List<Process> findByStatus(String status);
+
+    List<Process> findByCategory(String category);
+
+    List<Process> findByCompanyIdAndStatus(Long companyId, String status);
+}
